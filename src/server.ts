1592942c3873026a55919ee8439d6079bda02911
William Fank Thomé <willegp88@gmail.com>
2020-10-16 05:07:22 -0300

    refactor: use env variables

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,18 @@ import { MongoDB } from './infra/db/mongo/mongo.db'
 import { Fastify } from './main/web-servers/fastify/fastify.web-server'
 import { Application } from './main/app'
 
-const port = 5050
+// Vars
+const port = parseInt(process.env.PORT ?? '5050')
+const dbUrl = process.env.DB_URL ?? 'MONGO_URL'
+
+// Create 3rd party dependencies
 const webServer = new Fastify(port)
-const dbUrl = 'MONGO_URL'
 const db = new MongoDB(dbUrl)
+
+// Create app and inject dependencies
 const app = new Application(webServer, db)
 
+// Run app
 console.log('App starting')
 app.run().then(() => {
   console.log('App is running')
