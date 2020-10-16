b2118c52cd68cc6bf106dc13c0e15cd3129339b6
William Fank Thomé <willegp88@gmail.com>
2020-10-16 07:43:46 -0300

    chore: setup jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*.protocol.ts',
+    '!**/index.ts',
+    '!**/test/**'
+  ],
+  coverageDirectory: 'coverage',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
 };
