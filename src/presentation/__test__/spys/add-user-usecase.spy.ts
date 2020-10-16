8c19ce895f3129aa60515a57be9a35ce2f038115
William Fank Thomé <willegp88@gmail.com>
2020-10-16 18:18:24 -0300

    feat: create add user use case spy

---
+++ src/presentation/__test__/spys/add-user-usecase.spy.ts
@@ -0,0 +1,19 @@
+import { UserEntity } from '@/domain/entities'
+import { EntityDto } from '@/domain/protocols'
+import { AddUserUseCase } from '@/domain/usecases/user'
+import { mockUserEntity } from '../mocks/user-entity.mock'
+
+export class AddUserUseCaseSpy implements AddUserUseCase {
+  userDto?:  EntityDto<UserEntity>
+  userEntity?: UserEntity
+  shouldThrow = false
+
+  call = async (userDto: EntityDto<UserEntity>): Promise<UserEntity> => {
+    this.userDto = userDto
+
+    if (this.shouldThrow) throw new Error()
+
+    this.userEntity = mockUserEntity(userDto)
+    return this.userEntity
+  }
+}
