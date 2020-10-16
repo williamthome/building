497871078e2f879df352e1a07865aac753d93084
William Fank Thomé <willegp88@gmail.com>
2020-10-16 05:55:13 -0300

    refactor: remove unnecessary code

--- src/main/config/routes.config.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '../protocols/route.protocol'
-import { makeRoutes } from '../factories/route.factory'
-import { userRoutes } from '../routes/user.routes'
-
-export const setupRoutes = (): Array<Route<unknown>> => {
-  return makeRoutes(
-    ...userRoutes()
-  )
-}
