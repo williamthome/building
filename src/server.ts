309a312d08d4a8464bc6b85bd974f54059e23f52
William Fank Thomé <willegp88@gmail.com>
2020-10-16 21:30:39 -0300

    refactor: change server creation strategy

--- src/server.ts
+++ src/server.ts
@@ -3,21 +3,47 @@ import { MongoDB } from './infra/db/mongo/mongo.db'
 import { Fastify } from './main/web-servers/fastify/fastify.web-server'
 import { Application } from './main/app'
 
-// Vars
-const port = parseInt(process.env.PORT || '5050')
-const dbUrl = process.env.DB_URL || 'MONGO_URL'
+export type ServerStatus = 'undefined' | 'created' | 'listening' | 'error'
 
-// Create 3rd party dependencies
-const webServer = new Fastify(port)
-const db = new MongoDB(dbUrl)
+export class Server {
+  _status: ServerStatus = 'undefined'
+  app: Application
 
-// Create app and inject dependencies
-const app = new Application(webServer, db)
+  constructor () {
+    console.log('Creating server')
 
-// Run app
-console.log('App starting')
-app.run().then(() => {
-  console.log('App is running')
-}).catch((error) => {
-  console.error(error)
-})
+    // Vars
+    const port = parseInt(process.env.PORT || '5050')
+    const dbUrl = process.env.DB_URL || 'MONGO_URL'
+
+    // Create 3rd party dependencies
+    const webServer = new Fastify(port)
+    const db = new MongoDB(dbUrl)
+
+    // Create app and inject dependencies
+    this.app = new Application(webServer, db)
+
+    // Set status
+    this._status = 'created'
+
+    console.log('Server created')
+  }
+
+  get status(): ServerStatus {
+    return this._status
+  }
+
+  listen = async (): Promise<void> => {
+    try {
+      console.log('App starting')
+      await this.app.run()
+      this._status = 'listening'
+      console.log('App is running')
+    } catch (error) {
+      console.log('Shuting down')
+      this.app.stop()
+      this._status = 'error'
+      console.error(error)
+    }
+  }
+}
