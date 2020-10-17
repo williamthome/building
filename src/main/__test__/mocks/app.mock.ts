8194c3477dc27be68a7278f850a67dd0983859ec
William Fank Thomé <willegp88@gmail.com>
2020-10-17 02:21:32 -0300

    refactor: use a valid mongodb url

--- src/main/__test__/mocks/app.mock.ts
+++ src/main/__test__/mocks/app.mock.ts
@@ -3,7 +3,7 @@ import { Application } from '@/main/app'
 import { Fastify } from '@/main/web-servers/fastify/fastify.web-server'
 import { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify'
 
-export const mockApp = (port = 9200, dbUrl = 'dbUrl'):
+export const mockApp = (port = 9200, dbUrl = 'mongodb://localhost:27001,localhost:27002,localhost:27003/building'):
 Application<FastifyInstance, FastifyRequest, FastifyReply> => {
   const webServer = new Fastify(port)
   const db = new MongoDB(dbUrl)
--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { MongoDB } from './infra/db/mongo/mongo.db'
 
 // Vars
 const port = parseInt(process.env.PORT || '5050')
-const dbUrl = process.env.DB_URL || 'MONGO_URL'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27001,localhost:27002,localhost:27003/building'
 
 // Create 3rd party dependencies
 const webServer = new Fastify(port)
