3262bcc313a2944b961bdf6bfd5209ae74a4b107
William Fank Thomé <willegp88@gmail.com>
2020-10-18 02:48:37 -0300

    feat: create injector adapter

---
+++ src/shared/dependency-injection/adapters/injector.adapter.ts
@@ -0,0 +1,12 @@
+export type Constructor<T> = {
+  new(...args: any[]): T
+}
+
+export interface InjectorAdapter {
+  injectable: <T> () => (target: Constructor<T>) => void
+  isClassRegistered: <T> (target: Constructor<T>) => boolean
+  inject: <T> (token: string | symbol) => (target: Constructor<T>, propertyKey: string | symbol, parameterIndex: number) => void
+  isPropertyRegistered: <T> (token: string | symbol) => boolean
+  resolve: <T> (target: Constructor<T>) => T
+  clearInstances: () => void
+}
