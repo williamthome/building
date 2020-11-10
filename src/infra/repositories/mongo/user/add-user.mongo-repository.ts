e276442acb27207ca30ad30c759cf18814a07032
William Fank Thomé <willegp88@gmail.com>
2020-11-10 00:55:33 -0300

    refactor: inject mongodb and add user

--- src/infra/repositories/mongo/user/add-user.mongo-repository.ts
+++ src/infra/repositories/mongo/user/add-user.mongo-repository.ts
@@ -1,15 +1,18 @@
-import { Injectable } from 'heinjector'
+import { Inject, Injectable } from 'heinjector'
 import { ModelDto } from '@/data/protocols/model.protocol'
 import { UserModel } from '@/data/models/user.model'
 import { AddUserRepository } from '@/data/repositories/user/add-user.repository'
+import { MongoDB } from '@/infra/databases'
 
 @Injectable({
   identifier: 'addUserRepository'
 })
 export class AddUserMongoRepository implements AddUserRepository {
+  constructor (
+    @Inject({ identifier: MongoDB }) private readonly mongodb: MongoDB
+  ) {}
+
   addUser = async (userDto: ModelDto<UserModel>): Promise<UserModel> => {
-    return await Promise.resolve(new UserModel(
-      new Date(Date.now()).toISOString(), userDto.name!, userDto.address
-    ))
+    return await this.mongodb.addOne<UserModel>(userDto, 'users')
   }
 }
