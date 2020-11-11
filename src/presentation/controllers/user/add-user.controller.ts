e61f4ec6798a2ea80cb9f6c2e87fbd96900413ff
William Fank Thomé <willegp88@gmail.com>
2020-11-11 17:45:39 -0300

    refactor: user server error handler decorator

--- src/presentation/controllers/user/add-user.controller.ts
+++ src/presentation/controllers/user/add-user.controller.ts
@@ -2,9 +2,9 @@
 import { Inject } from '@/shared/dependency-injection'
 // > In: presentation layer
 import { Controller, HandleResponse, HttpRequest } from '@/presentation/protocols'
-import { ok, serverError } from '@/presentation/factories/http.factory'
+import { ok } from '@/presentation/factories/http.factory'
 import { userSchema } from '@/presentation/schemas/user.schema'
-import { InjectableController, ValidateRequest } from '@/presentation/decorators'
+import { InjectableController, ServerErrorHandler, ValidateRequest } from '@/presentation/decorators'
 // < Out: only domain layer
 import { UserEntity, userKeys } from '@/domain/entities'
 import { AddUserUseCase } from '@/domain/usecases/user'
@@ -21,20 +21,15 @@ export class AddUserController implements Controller<UserEntity> {
     @Inject() private readonly addUserUseCase: AddUserUseCase
   ) { }
 
+  @ServerErrorHandler
   @ValidateRequest<AddUserDto, UserEntity>({
     schema: userSchema,
     keys: userKeys,
     nullable: false
   })
-  async handle(request: HttpRequest<AddUserDto>): HandleResponse<UserEntity> {
-    try {
-      const userDto = request.body as AddUserDto
-      const newUser = await this.addUserUseCase.call(userDto)
-
-      return ok(newUser)
-    } catch (error) {
-      console.error(error)
-      return serverError(error)
-    }
+  async handle (request: HttpRequest<AddUserDto>): HandleResponse<UserEntity> {
+    const userDto = request.body as AddUserDto
+    const newUser = await this.addUserUseCase.call(userDto)
+    return ok(newUser)
   }
 }
