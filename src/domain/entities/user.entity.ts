0eb460c9c025fa167f8d770cfc5b7a64e701fe36
William Fank Thomé <willegp88@gmail.com>
2020-11-11 15:05:06 -0300

    refactor: create user keys

--- src/domain/entities/user.entity.ts
+++ src/domain/entities/user.entity.ts
@@ -1,7 +1,15 @@
+import { DeepFlattenPaths } from '@/shared/types/flatten'
 import { Entity } from '../protocols/entity.protocol'
-import { Address } from './nested'
+import { Address, addressKeys } from './nested'
 
 export interface UserEntity extends Entity {
   name: string
   address?: Address
+}
+
+export const userKeys: DeepFlattenPaths<UserEntity> = {
+  id: 'id',
+  name: 'name',
+  address: 'address',
+  ...addressKeys
 }
