228620aa5e9ece8ab66eb5d688cb4812bc4b71c7
William Fank Thomé <willegp88@gmail.com>
2020-11-11 15:04:25 -0300

    refactor: create address keys

--- src/domain/entities/nested/address.nested.ts
+++ src/domain/entities/nested/address.nested.ts
@@ -1,5 +1,13 @@
+import { DeepFlattenPaths } from '@/shared/types'
+
 export interface Address {
   street?: string
   city?: string
   state?: string
+}
+
+export const addressKeys: DeepFlattenPaths<Address> = {
+  street: 'street',
+  city: 'city',
+  state: 'state'
 }
