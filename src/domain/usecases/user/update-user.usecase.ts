a9aa3aad796dd1f1b9317647d603a93c81c02852
William Fank Thomé <willegp88@gmail.com>
2020-11-12 14:16:47 -0300

    feat: create update user usecase

---
+++ src/domain/usecases/user/update-user.usecase.ts
@@ -0,0 +1,6 @@
+import { UserEntity } from '@/domain/entities'
+import { UserDto } from '@/domain/protocols'
+
+export interface UpdateUserUseCase {
+  call: (userId: UserEntity['id'], userDto: UserDto) => Promise<UserEntity>
+}
