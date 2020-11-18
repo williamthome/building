5460c75eb55ab781e5314a4017319ca66843c659
William Fank Thomé <willegp88@gmail.com>
2020-11-18 09:55:20 -0300

    test: cryptography adapters

---
+++ src/__tests__/infra/cryptography/bcrypt.adapter.spec.ts
@@ -0,0 +1,85 @@
+import bcrypt from 'bcrypt'
+// : Shared
+import fakeData from '../../utils/fakeData'
+// > In: infra layer
+import { BcryptAdapter } from '@/infra/cryptography'
+// < Out: only data layer
+
+// #region Factory
+
+const plaintext = fakeData.entity.password()
+const digest = `${plaintext}_hashed`
+
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return await Promise.resolve(digest)
+  },
+  async compare (): Promise<boolean> {
+    return await Promise.resolve(true)
+  }
+}))
+
+interface SutTypes {
+  sut: BcryptAdapter
+}
+
+const salt = 12
+const makeSut = (): SutTypes => {
+  const sut = new BcryptAdapter(salt)
+  return {
+    sut
+  }
+}
+
+// #endregion Factory
+
+describe('Bcrypt Adapter', () => {
+  describe('hash()', () => {
+    test('Should call method with right values', async () => {
+      const { sut } = makeSut()
+      const hashSpy = jest.spyOn(bcrypt, 'hash')
+      await sut.hash(plaintext)
+      expect(hashSpy).toHaveBeenCalledWith(plaintext, salt)
+    })
+
+    test('Should returns a hash on method success', async () => {
+      const { sut } = makeSut()
+      const hash = await sut.hash(plaintext)
+      expect(hash).toBe(digest)
+    })
+
+    test('Should throw if method throws', async () => {
+      const { sut } = makeSut()
+      jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => { throw new Error() })
+      await expect(sut.hash(plaintext)).rejects.toThrow()
+    })
+  })
+
+  describe('compare()', () => {
+    test('Should call method with right values', async () => {
+      const { sut } = makeSut()
+      const compareSpy = jest.spyOn(bcrypt, 'compare')
+      await sut.match(plaintext, plaintext)
+      expect(compareSpy).toHaveBeenCalledWith(plaintext, plaintext)
+    })
+
+    test('Should returns true on success', async () => {
+      const { sut } = makeSut()
+      const match = await sut.match(plaintext, plaintext)
+      expect(match).toBe(true)
+    })
+
+    test('Should returns false on fails', async () => {
+      const { sut } = makeSut()
+      jest.spyOn(bcrypt, 'compare').mockResolvedValue(false)
+      const match = await sut.match(plaintext, plaintext)
+      expect(match).toBe(false)
+    })
+
+    test('Should throw if method throws', async () => {
+      const { sut } = makeSut()
+      jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => { throw new Error() })
+      await expect(sut.match(plaintext, plaintext)).rejects.toThrow()
+    })
+  })
+})
---
+++ src/__tests__/infra/cryptography/jwt.adapter.spec.ts
@@ -0,0 +1,81 @@
+import jwt from 'jsonwebtoken'
+// : Shared
+import fakeData from '../../utils/fakeData'
+// > In: infra layer
+import { JwtAdapter } from '@/infra/cryptography'
+// < Out: only data layer
+
+// #region Factories
+
+const fakeSecret = 'jwtSecret'
+const fakeToken = fakeData.entity.token()
+const fakeVerifiedToken = fakeData.entity.token()
+const fakeId = fakeData.entity.id()
+
+jest.mock('jsonwebtoken', () => ({
+  sign (): string {
+    return fakeToken
+  },
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  verify (): string | object {
+    return fakeVerifiedToken
+  }
+}))
+
+interface SutTypes {
+  sut: JwtAdapter
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new JwtAdapter(fakeSecret)
+  return {
+    sut
+  }
+}
+
+// #endregion Factories
+
+describe('JWT Adapter', () => {
+  describe('sign()', () => {
+    test('Should call method with right values', async () => {
+      const { sut } = makeSut()
+      const signSpy = jest.spyOn(jwt, 'sign')
+      await sut.encrypt(fakeId)
+      expect(signSpy).toHaveBeenCalledWith({ id: fakeId }, fakeSecret)
+    })
+
+    test('Should returns a token on success', async () => {
+      const { sut } = makeSut()
+      const accessToken = await sut.encrypt(fakeId)
+      expect(accessToken).toBe(fakeToken)
+    })
+
+    test('Should throw if method throws', async () => {
+      const { sut } = makeSut()
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() })
+      await expect(sut.encrypt(fakeId)).rejects.toThrow()
+    })
+  })
+
+  describe('verify()', () => {
+    test('Should call method with right values', async () => {
+      const { sut } = makeSut()
+      const verifySpy = jest.spyOn(jwt, 'verify')
+      await sut.decrypt(fakeToken)
+      expect(verifySpy).toHaveBeenCalledWith(fakeToken, fakeSecret)
+    })
+
+    test('Should returns a decrypted string on success', async () => {
+      const { sut } = makeSut()
+      const decryptedToken = await sut.decrypt(fakeId)
+      expect(decryptedToken).toBe(decryptedToken)
+    })
+
+    test('Should throw if method throws', async () => {
+      const { sut } = makeSut()
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => { throw new Error() })
+      await expect(sut.decrypt(fakeId)).rejects.toThrow()
+    })
+  })
+})
