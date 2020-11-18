cae35eaa10ab8c550e2b13f6ef9e2bc5d33afb32
William Fank Thomé <willegp88@gmail.com>
2020-11-18 10:51:39 -0300

    refactor: improve code

--- src/data/contracts/user/add-user.contract.ts
+++ src/data/contracts/user/add-user.contract.ts
@@ -2,11 +2,11 @@
 import { Injectable, Inject } from '@/shared/dependency-injection'
 // > Data
 import { AddUserRepository } from '@/data/repositories/user'
+import { Hasher } from '@/data/protocols/cryptography'
 // < Only Domain
 import { UserEntity } from '@/domain/entities'
 import { AddUserUseCase } from '@/domain/usecases/user'
 import { UserDto } from '@/domain/protocols'
-import { Hasher } from '@/data/protocols/cryptography'
 
 @Injectable('addUserUseCase')
 export class AddUserContract implements AddUserUseCase {
@@ -17,8 +17,7 @@ export class AddUserContract implements AddUserUseCase {
   ) {}
 
   call = async (userDto: UserDto): Promise<UserEntity> => {
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const hashedPassword = await this.hasher.hash(userDto.password!)
+    const hashedPassword = await this.hasher.hash(userDto.password || '')
     return await this.addUserRepository.addUser({
       ...userDto,
       password: hashedPassword
