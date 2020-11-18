0554373c0a70c407996485480a52ea10416cf1de
William Fank Thomé <willegp88@gmail.com>
2020-11-18 10:43:41 -0300

    refactor: add password field to mock user model dto

--- src/__tests__/data/__mocks__/user-model-dto.mock.ts
+++ src/__tests__/data/__mocks__/user-model-dto.mock.ts
@@ -4,6 +4,7 @@ import { ModelDto } from '@/data/protocols/model.protocol'
 
 export const mockUserModelDto = (): ModelDto<UserModel> => ({
   name: fakeData.person.fullName(),
+  password: fakeData.entity.password(),
   address: {
     street: fakeData.address.street(),
     city: fakeData.address.city(),
