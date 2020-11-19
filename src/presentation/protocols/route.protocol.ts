6a5b197a361b4254ca5d5f321d6291f1474f4d93
William Fank Thomé <willegp88@gmail.com>
2020-11-19 17:32:24 -0300

    feat: create route requirement type

--- src/presentation/protocols/route.protocol.ts
+++ src/presentation/protocols/route.protocol.ts
@@ -1,9 +1,11 @@
 import { Controller, HttpMethods } from '../protocols'
 
+export type RouteRequirement = 'admin' | 'auth' | 'master' | 'none'
+
 export interface Route<T> {
   method: HttpMethods
   path: string
   controller: Controller<T>
-  requirement: 'admin' | 'auth' | 'master' | 'none'
+  requirement: RouteRequirement
   permissions?: number
 }
