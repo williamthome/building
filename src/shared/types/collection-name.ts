fe4f95d30691b248241903fff72e996b3cdecf8e
William Fank Thomé <willegp88@gmail.com>
2020-11-20 12:22:25 -0300

    feat: create collection name type

---
+++ src/shared/types/collection-name.ts
@@ -0,0 +1,3 @@
+export type CollectionName =
+ | 'users'
+ | 'companies'
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,3 +1,4 @@
 export * from './omit-on-indexed'
 export * from './flatten'
 export * from './user-role'
+export * from './collection-name'
