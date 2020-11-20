6dd4acf3b73759ac996dc12f8f677a0ccdf99b15
William Fank Thomé <willegp88@gmail.com>
2020-11-20 17:48:58 -0300

    refactor: middleware returns logged user info or error

--- src/presentation/middlewares/auth.middleware.ts
+++ src/presentation/middlewares/auth.middleware.ts
@@ -2,8 +2,8 @@ import { GetUserByAccessTokenUseCase } from '@/domain/usecases'
 import { Inject, Injectable } from '@/shared/dependency-injection'
 import { HttpHeaderName } from '../constants'
 import { AccessDeniedError, CanNotFindEntityError } from '../errors'
-import { forbidden, notFound, ok } from '../factories/http.factory'
-import { HttpRequest, HttpResponse, Middleware } from '../protocols'
+import { forbidden, notFound, ok, serverError } from '../factories/http.factory'
+import { HttpRequest, HttpResponse, LoggedUserInfo, Middleware } from '../protocols'
 
 @Injectable()
 export class AuthMiddleware implements Middleware {
@@ -12,22 +12,32 @@ export class AuthMiddleware implements Middleware {
     @Inject() private readonly getUserByAccessTokenUseCase: GetUserByAccessTokenUseCase
   ) { }
 
-  handle = async (httpRequest: HttpRequest): Promise<HttpResponse> => {
-    const bearer: string | undefined =
-      httpRequest.headers?.[HttpHeaderName.AUTHORIZATION] ||
-      httpRequest.headers?.[HttpHeaderName.AUTHORIZATION.toLowerCase()]
+  handle = async (httpRequest: HttpRequest): Promise<HttpResponse<LoggedUserInfo | Error>> => {
+    try {
+      const bearer: string | undefined =
+        httpRequest.headers?.[HttpHeaderName.AUTHORIZATION] ||
+        httpRequest.headers?.[HttpHeaderName.AUTHORIZATION.toLowerCase()]
 
-    if (!bearer || !bearer.startsWith('Bearer '))
-      return forbidden(new AccessDeniedError())
+      if (!bearer || !bearer.startsWith('Bearer '))
+        return forbidden(new AccessDeniedError())
 
-    const accessToken = bearer.substring(7)
+      const accessToken = bearer.substring(7)
 
-    const user  = await this.getUserByAccessTokenUseCase.call(accessToken)
-    if (!user)
-      return notFound(new CanNotFindEntityError('User'))
+      const user  = await this.getUserByAccessTokenUseCase.call(accessToken)
+      if (!user)
+        return notFound(new CanNotFindEntityError('User'))
 
-    httpRequest.loggedUserInfo.id = user.id
+      if (httpRequest.loggedUserInfo) {
+        httpRequest.loggedUserInfo.id = user.id
+      } else {
+        httpRequest.loggedUserInfo = {
+          id: user.id
+        }
+      }
 
-    return ok(httpRequest)
+      return ok(httpRequest.loggedUserInfo)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
--- src/presentation/protocols/middleware.protocol.ts
+++ src/presentation/protocols/middleware.protocol.ts
@@ -1,5 +1,6 @@
 import { HttpRequest, HttpResponse } from './http.protocol'
+import { LoggedUserInfo } from './logged-user-info.protocol'
 
 export interface Middleware {
-  handle: (httpRequest: HttpRequest) => Promise<HttpResponse>
+  handle: (httpRequest: HttpRequest) => Promise<HttpResponse<LoggedUserInfo | Error>>
 }
