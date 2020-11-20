224afa45e6ca6475b804e8bea1e9912d3e6a6323
William Fank Thomé <willegp88@gmail.com>
2020-11-20 12:29:29 -0300

    refactor: create company keys

--- src/domain/entities/company.entity.ts
+++ src/domain/entities/company.entity.ts
@@ -1,7 +1,14 @@
+import { DeepFlattenPaths } from '@/shared/types'
 import { Entity } from '../protocols'
 import { Member } from './nested'
 
 export interface CompanyEntity extends Entity {
   name: string
   members: Member[]
+}
+
+export const companyKeys: DeepFlattenPaths<CompanyEntity> = {
+  id: 'id',
+  name: 'name',
+  members: 'members'
 }
