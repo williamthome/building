f0f15b87719141b5f2290872ee554caa4a6d9509
William Fank Thomé <willegp88@gmail.com>
2020-11-20 15:49:40 -0300

    refactor: use lower case for entity name

--- src/presentation/errors/can-not-find-entity.error.ts
+++ src/presentation/errors/can-not-find-entity.error.ts
@@ -2,7 +2,7 @@ import { EntityName } from '../constants'
 
 export class CanNotFindEntityError extends Error {
   constructor (entityName: EntityName) {
-    super(`Can not find ${entityName}`)
+    super(`Can not find ${entityName.toLowerCase()}`)
     this.name = 'CanNotFindEntityError'
   }
 }
