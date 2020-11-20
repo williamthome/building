2cdaea42c7c952d0893e2ce1fe309b2173e2bb54
William Fank Thomé <willegp88@gmail.com>
2020-11-20 12:33:48 -0300

    refactor: improve imports

--- src/presentation/controllers/user/add-user.controller.ts
+++ src/presentation/controllers/user/add-user.controller.ts
@@ -3,11 +3,11 @@ import { Inject, Injectable } from '@/shared/dependency-injection'
 // > In: presentation layer
 import { Controller, HandleResponse, HttpRequest } from '@/presentation/protocols'
 import { ok } from '@/presentation/factories/http.factory'
-import { userSchema } from '@/presentation/schemas/user.schema'
+import { userSchema } from '@/presentation/schemas'
 import { ServerErrorHandler, ValidateRequest } from '@/presentation/decorators'
 // < Out: only domain layer
 import { UserEntity, userKeys } from '@/domain/entities'
-import { AddUserUseCase } from '@/domain/usecases/user'
+import { AddUserUseCase } from '@/domain/usecases'
 import { UserDto } from '@/domain/protocols'
 
 @Injectable()
