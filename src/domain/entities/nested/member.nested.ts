071fd21a39117dd15a4dbce67f527b273ee0ab25
William Fank Thomé <willegp88@gmail.com>
2020-11-20 12:28:46 -0300

    refactor: create member keys

--- src/domain/entities/nested/member.nested.ts
+++ src/domain/entities/nested/member.nested.ts
@@ -1,9 +1,15 @@
-import { UserEntity } from '../user.entity'
+import { DeepFlattenPaths, UserRole } from '@/shared/types'
 import { UserFeatures } from '@/shared/constants'
-import { UserRole } from '@/shared/types'
+import { UserEntity } from '../user.entity'
 
 export interface Member {
   userId: UserEntity['id']
   role: UserRole
   features: UserFeatures
+}
+
+export const memberKeys: DeepFlattenPaths<Member> = {
+  userId: 'userId',
+  role: 'role',
+  features: 'features'
 }
