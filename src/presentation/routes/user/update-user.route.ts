7a28fc61e1a050684cb84ac31c86b75fe190c799
William Fank Thomé <willegp88@gmail.com>
2020-11-20 16:15:01 -0300

    refactor: use auth middleware for update user route

--- src/presentation/routes/user/update-user.route.ts
+++ src/presentation/routes/user/update-user.route.ts
@@ -1,16 +1,18 @@
 import { UserEntity } from '@/domain/entities'
 import { UpdateUserController } from '@/presentation/controllers'
+import { AuthMiddleware } from '@/presentation/middlewares'
 import { Controller, HttpMethods, Middleware, Route, RouteRequirement } from '@/presentation/protocols'
 import { Inject, InjectableArray } from '@/shared/dependency-injection'
 
 @InjectableArray('routes')
 export class UpdateUserRoute implements Route<UserEntity> {
   constructor (
-    @Inject(UpdateUserController) public readonly controller: Controller<UserEntity>
+    @Inject(UpdateUserController) public readonly controller: Controller<UserEntity>,
+    @Inject(AuthMiddleware) public readonly authMiddleware: Middleware
   ) { }
 
   get method(): HttpMethods { return 'PATCH' }
   get path(): string { return '/user/:id' }
   get requirement(): RouteRequirement { return 'none' }
-  get middlewares(): Middleware[] { return [] }
+  get middlewares(): Middleware[] { return [this.authMiddleware] }
 }
