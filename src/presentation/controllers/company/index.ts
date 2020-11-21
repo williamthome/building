70b0bb5ca497a65e4d2d16335e5bb150417ba984
William Fank Thomé <willegp88@gmail.com>
2020-11-21 12:05:17 -0300

    feat: create update company controller

--- src/presentation/controllers/company/index.ts
+++ src/presentation/controllers/company/index.ts
@@ -1,1 +1,2 @@
 export * from './add-company.controller'
+export * from './update-company.controller'
---
+++ src/presentation/controllers/company/update-company.controller.ts
@@ -0,0 +1,41 @@
+// : Shared
+import { Inject, Injectable } from '@/shared/dependency-injection'
+// > In: presentation layer
+import { Controller, HandleResponse, HttpRequest } from '@/presentation/protocols'
+import { badRequest, notFound, ok } from '@/presentation/factories/http.factory'
+import { companySchema } from '@/presentation/schemas'
+import { ServerErrorHandler, ValidateRequest } from '@/presentation/decorators'
+import { CanNotFindEntityError, MissingParamError } from '@/presentation/errors'
+// < Out: only domain layer
+import { CompanyEntity, companyKeys } from '@/domain/entities'
+import { UpdateCompanyUseCase } from '@/domain/usecases'
+import { CompanyDto } from '@/domain/protocols'
+
+@Injectable()
+export class UpdateCompanyController implements Controller<CompanyEntity> {
+
+  constructor (
+    @Inject() private readonly updateCompanyUseCase: UpdateCompanyUseCase
+  ) { }
+
+  @ValidateRequest<CompanyDto, CompanyEntity>({
+    schema: companySchema,
+    keys: companyKeys,
+    nullable: true
+  })
+  @ServerErrorHandler
+  async handle (request: HttpRequest<CompanyDto>): HandleResponse<CompanyEntity> {
+    const id = request.params?.id
+
+    if (!id)
+      return badRequest(new MissingParamError('id'))
+
+    const companyDto = request.body as CompanyDto
+    const udpatedCompany = await this.updateCompanyUseCase.call(id, companyDto)
+
+    if (!udpatedCompany)
+      return notFound(new CanNotFindEntityError('Company'))
+
+    return ok(udpatedCompany)
+  }
+}
