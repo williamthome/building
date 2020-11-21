e090cbfcde132ea44c3159c4c12befcbe724d9f7
William Fank Thomé <willegp88@gmail.com>
2020-11-20 22:31:27 -0300

    feat: create get user by access token repository spy

---
+++ src/__tests__/data/__spys__/repositories/user/get-user-by-access-token-repository.spy.ts
@@ -0,0 +1,22 @@
+import { UserModel } from '@/data/models'
+import { GetUserByAccessTokenRepository } from '@/data/repositories/user'
+import { mockUserModel } from '@/__tests__/data/__mocks__/models'
+
+export class GetUserByAccessTokenRepositorySpy implements GetUserByAccessTokenRepository {
+  accessToken?: UserModel['accessToken']
+  userModel?: UserModel | null
+  shouldReturnNull = false
+  shouldThrow = false
+
+  getUserByAccessToken = async (accessToken: UserModel['accessToken']): Promise<UserModel | null> => {
+    this.accessToken = accessToken
+
+    if (this.shouldThrow) throw new Error()
+
+    this.userModel = this.shouldReturnNull
+      ? null
+      : { ...mockUserModel(), accessToken }
+
+    return this.userModel
+  }
+}
