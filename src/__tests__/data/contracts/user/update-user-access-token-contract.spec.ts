eca8d813c3f550c83808579cb10bff456e2511d0
William Fank Thomé <willegp88@gmail.com>
2020-11-20 22:54:15 -0300

    test: update user access token contract

---
+++ src/__tests__/data/contracts/user/update-user-access-token-contract.spec.ts
@@ -0,0 +1,52 @@
+import container from '@/shared/dependency-injection'
+import { UpdateUserAccessTokenContract } from '@/data/contracts'
+import { UpdateUserAccessTokenRepositorySpy } from '@/__tests__/data/__spys__'
+import fakeData from '@/__tests__/shared/fake-data'
+
+//#region Factories
+
+interface SutTypes {
+  sut: UpdateUserAccessTokenContract
+  updateUserAccessTokenRepositorySpy: UpdateUserAccessTokenRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const updateUserAccessTokenRepositorySpy = container.resolve<UpdateUserAccessTokenRepositorySpy>('updateUserAccessTokenRepository')
+  const sut = container.resolve(UpdateUserAccessTokenContract)
+  return {
+    sut,
+    updateUserAccessTokenRepositorySpy
+  }
+}
+
+//#endregion Factories
+
+describe('UpdateUserAccessToken Contract', () => {
+  beforeEach(() => {
+    container.clear()
+    container.bind('updateUserAccessTokenRepository').asNewable(UpdateUserAccessTokenRepositorySpy)
+    container.bind(UpdateUserAccessTokenContract).asNewable(UpdateUserAccessTokenContract)
+  })
+
+  describe('UpdateUserAccessToken Repository', () => {
+    it('should be called with right value', async () => {
+      const { sut, updateUserAccessTokenRepositorySpy } = makeSut()
+      const id = fakeData.entity.id()
+      const accessToken = fakeData.entity.token()
+      await sut.call(id, accessToken)
+      expect(updateUserAccessTokenRepositorySpy.id).toEqual(id)
+      expect(updateUserAccessTokenRepositorySpy.accessToken).toEqual(accessToken)
+    })
+
+    it('should throw if method throws', async () => {
+      const { sut, updateUserAccessTokenRepositorySpy } = makeSut()
+      updateUserAccessTokenRepositorySpy.shouldThrow = true
+      await expect(sut.call(fakeData.entity.id(), fakeData.entity.token())).rejects.toThrow()
+    })
+  })
+
+  it('shold not throw on update user access token', async () => {
+    const { sut } = makeSut()
+    await expect(sut.call(fakeData.entity.id(), fakeData.entity.token())).resolves.not.toThrow()
+  })
+})
