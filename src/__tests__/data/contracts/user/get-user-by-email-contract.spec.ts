8511258a4185e5b0fee08abfde96e658cac7f0a0
William Fank Thomé <willegp88@gmail.com>
2020-11-20 23:08:49 -0300

    test: get user by email contract

---
+++ src/__tests__/data/contracts/user/get-user-by-email-contract.spec.ts
@@ -0,0 +1,58 @@
+import container from '@/shared/dependency-injection'
+import { GetUserByEmailContract } from '@/data/contracts'
+import { GetUserByEmailRepositorySpy } from '@/__tests__/data/__spys__'
+import fakeData from '@/__tests__/shared/fake-data'
+
+//#region Factories
+
+interface SutTypes {
+  sut: GetUserByEmailContract
+  getUserByEmailRepositorySpy: GetUserByEmailRepositorySpy
+}
+
+const makeSut = (): SutTypes => {
+  const getUserByEmailRepositorySpy = container.resolve<GetUserByEmailRepositorySpy>('getUserByEmailRepository')
+  const sut = container.resolve(GetUserByEmailContract)
+  return {
+    sut,
+    getUserByEmailRepositorySpy
+  }
+}
+
+//#endregion Factories
+
+describe('GetUserByEmail Contract', () => {
+  beforeEach(() => {
+    container.clear()
+    container.bind('getUserByEmailRepository').asNewable(GetUserByEmailRepositorySpy)
+    container.bind(GetUserByEmailContract).asNewable(GetUserByEmailContract)
+  })
+
+  describe('GetUserByEmail Repository', () => {
+    it('should be called with right value', async () => {
+      const { sut, getUserByEmailRepositorySpy } = makeSut()
+      const email = fakeData.person.email()
+      const user = await sut.call(email)
+      expect(getUserByEmailRepositorySpy.email).toEqual(email)
+      expect(getUserByEmailRepositorySpy.userModel).toEqual(user)
+    })
+
+    it('should throw if method throws', async () => {
+      const { sut, getUserByEmailRepositorySpy } = makeSut()
+      getUserByEmailRepositorySpy.shouldThrow = true
+      await expect(sut.call(fakeData.person.email())).rejects.toThrow()
+    })
+
+    it('should return null if user not found', async () => {
+      const { sut, getUserByEmailRepositorySpy } = makeSut()
+      getUserByEmailRepositorySpy.shouldReturnNull = true
+      await expect(sut.call(fakeData.person.email())).resolves.toBeNull()
+    })
+  })
+
+  it('shold return user', async () => {
+    const { sut } = makeSut()
+    const user = await sut.call(fakeData.person.email())
+    expect(user).toBeTruthy()
+  })
+})
