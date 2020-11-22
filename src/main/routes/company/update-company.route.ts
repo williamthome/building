1bfa4de47e800ca97ada7fbc516d9d15bb50fc10
William Fank Thomé <willegp88@gmail.com>
2020-11-22 15:04:30 -0300

    refactor: add route middleware in update company route middlewares

--- src/main/routes/company/update-company.route.ts
+++ src/main/routes/company/update-company.route.ts
@@ -1,19 +1,27 @@
 import { Inject, InjectableArray } from '@/shared/dependency-injection'
-import { AuthMiddleware } from '@/main/middlewares'
+import { AuthMiddleware, RoleMiddleware } from '@/main/middlewares'
 import { Middleware, Route, RouteRequirement } from '@/main/protocols'
 import { UpdateCompanyController } from '@/presentation/controllers'
 import { Controller, HttpMethods } from '@/presentation/protocols'
 import { CompanyEntity } from '@/domain/entities'
+import { UserRole } from '@/shared/constants'
 
 @InjectableArray('routes')
 export class UpdateCompanyRoute implements Route<CompanyEntity> {
+  roleMiddleware = new RoleMiddleware([UserRole.owner, UserRole.admin])
+
   constructor (
     @Inject(UpdateCompanyController) public readonly controller: Controller<CompanyEntity>,
     @Inject(AuthMiddleware) public readonly authMiddleware: Middleware
   ) { }
 
-  get method(): HttpMethods { return 'PATCH' }
-  get path(): string { return '/company/:id' }
-  get requirement(): RouteRequirement { return 'none' }
-  get middlewares(): Middleware[] { return [this.authMiddleware] }
+  get method (): HttpMethods { return 'PATCH' }
+  get path (): string { return '/company/:id' }
+  get requirement (): RouteRequirement { return 'none' }
+  get middlewares (): Middleware[] {
+    return [
+      this.authMiddleware,
+      this.roleMiddleware
+    ]
+  }
 }
