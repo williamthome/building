be400440ad97cf535373bb2a8b4cf5006ab79e34
William Fank Thomé <willegp88@gmail.com>
2020-11-22 15:02:08 -0300

    refactor: change logged user info strategy

--- src/presentation/protocols/logged-user-info.protocol.ts
+++ src/presentation/protocols/logged-user-info.protocol.ts
@@ -1,9 +1,15 @@
 import { UserEntity } from '@/domain/entities'
+import { Member } from '@/domain/entities/nested'
 
 export type LoggedUserInfo =
   Partial<
     Pick<UserEntity,
       | 'id'
-      | 'activeCompanyId'
+    >
+  >
+  & Partial<
+    Pick<Member,
+      | 'role'
+      | 'features'
     >
   >
