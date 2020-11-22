1c067d59f85ac19779cf47bbc4adbb6b77f3229e
William Fank Thomé <willegp88@gmail.com>
2020-11-22 17:51:28 -0300

    test: requirements middleware

---
+++ src/__tests__/main/middlewares/requirements-middleware.spec.ts
@@ -0,0 +1,79 @@
+import { AllUserFeatures, CompanyRole, UserFeatures } from '@/shared/constants'
+import { RequirementsMiddleware } from '@/main/middlewares'
+import { noContent, unauthorized } from '@/presentation/factories/http.factory'
+
+//#region Factories
+
+interface SutTypes {
+  sut: RequirementsMiddleware
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new RequirementsMiddleware(AllUserFeatures)
+  return {
+    sut
+  }
+}
+
+//#endregion Factories
+
+describe('Requirements Middleware', () => {
+  it('should return unauthorized if logged user info is undefined', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle({})
+    expect(response).toEqual(unauthorized())
+  })
+
+  it('should return unauthorized if company role is undefined', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle({
+      loggedUserInfo: {
+        features: AllUserFeatures
+      }
+    })
+    expect(response).toEqual(unauthorized())
+  })
+
+  it('should return unauthorized if features are undefined', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle({
+      loggedUserInfo: {
+        companyRole: CompanyRole.owner
+      }
+    })
+    expect(response).toEqual(unauthorized())
+  })
+
+  it('should return unauthorized if insuficient requirements', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle({
+      loggedUserInfo: {
+        companyRole: CompanyRole.employee,
+        features: UserFeatures.None
+      }
+    })
+    expect(response).toEqual(unauthorized())
+  })
+
+  it('should return no content if is company owner', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle({
+      loggedUserInfo: {
+        companyRole: CompanyRole.owner,
+        features: UserFeatures.None
+      }
+    })
+    expect(response).toEqual(noContent())
+  })
+
+  it('should return no content if has requirements', async () => {
+    const { sut } = makeSut()
+    const response = await sut.handle({
+      loggedUserInfo: {
+        companyRole: CompanyRole.employee,
+        features: AllUserFeatures
+      }
+    })
+    expect(response).toEqual(noContent())
+  })
+})
