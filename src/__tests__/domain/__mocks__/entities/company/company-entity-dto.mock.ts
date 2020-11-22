16ce3c7be8eb7587af2412d7097b4eec6ace5a34
William Fank Thomé <willegp88@gmail.com>
2020-11-22 19:11:34 -0300

    refactor: add owner option for company entity dto mock

--- src/__tests__/domain/__mocks__/entities/company/company-entity-dto.mock.ts
+++ src/__tests__/domain/__mocks__/entities/company/company-entity-dto.mock.ts
@@ -1,11 +1,11 @@
 import fakeData from '@/__tests__/shared/fake-data'
-import { CompanyEntity } from '@/domain/entities'
+import { CompanyEntity, UserEntity } from '@/domain/entities'
 import { CompanyRole, UserFeatures } from '@/shared/constants'
 
-export const mockCompanyEntityDto = (): Omit<CompanyEntity, 'id'> => ({
+export const mockCompanyEntityDto = (owner?: UserEntity): Omit<CompanyEntity, 'id'> => ({
   name: fakeData.person.fullName(),
   members: [{
-    userId: fakeData.entity.id(),
+    userId: owner ? owner.id : fakeData.entity.id(),
     companyRole: CompanyRole.owner,
     features: UserFeatures.None
   }]
