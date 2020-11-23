299d30a911097776e716c4bdd35ef782c43a984a
William Fank Thomé <willegp88@gmail.com>
2020-11-23 10:34:58 -0300

    feat: create add company member contract

---
+++ src/data/contracts/company/add-company-member.contract.ts
@@ -0,0 +1,20 @@
+// : Shared
+import { Injectable, Inject } from '@/shared/dependency-injection'
+// > Data
+import { AddCompanyMemberRepository } from '@/data/repositories'
+// < Only Domain
+import { CompanyEntity } from '@/domain/entities'
+import { AddCompanyMemberUseCase } from '@/domain/usecases'
+import { Member } from '@/domain/entities/nested'
+
+@Injectable('addCompanyMemberUseCase')
+export class AddCompanyMemberContract implements AddCompanyMemberUseCase {
+
+  constructor (
+    @Inject() private readonly addCompanyMemberRepository: AddCompanyMemberRepository
+  ) {}
+
+  call = async (companyId: CompanyEntity['id'], member: Member): Promise<CompanyEntity | null> => {
+    return await this.addCompanyMemberRepository.addCompanyMember(companyId, member)
+  }
+}
--- src/data/contracts/company/index.ts
+++ src/data/contracts/company/index.ts
@@ -1,3 +1,4 @@
 export * from './add-company.contract'
 export * from './update-company.contract'
 export * from './get-company-by-id.contract'
+export * from './add-company-member.contract'
