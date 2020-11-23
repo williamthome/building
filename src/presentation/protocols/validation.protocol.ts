87e9830b4ea83753393ee2e33692749d10b53a37
William Fank Thomé <willegp88@gmail.com>
2020-11-23 14:25:07 -0300

    refactor: create base validation

--- src/presentation/protocols/validation.protocol.ts
+++ src/presentation/protocols/validation.protocol.ts
@@ -7,3 +7,21 @@ export interface ValidationResult {
 export interface Validation {
   validate: <T extends Record<string, unknown>> (obj: T, field: keyof T, validations?: Validation[], customErrorMessage?: string) => ValidationResult
 }
+
+export abstract class BaseValidation<T extends Validation> implements Validation {
+  isParam = false
+  customErrorMessage: string | undefined = undefined
+  abstract readonly validation: () => T
+
+  param = (): T => {
+    this.isParam = true
+    return this.validation()
+  }
+
+  message = (message: string | undefined): T => {
+    this.customErrorMessage = message
+    return this.validation()
+  }
+
+  abstract validate: <T extends Record<string, unknown>> (obj: T, field: keyof T, validations?: Validation[], customErrorMessage?: string) => ValidationResult
+}
