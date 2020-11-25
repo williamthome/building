54a859ee769dbc45193d4d81dc80827d38792640
William Fank Thomé <willegp88@gmail.com>
2020-11-25 15:55:09 -0300

    test: update company route

---
+++ src/__tests__/main/routes/company/update-company-route.test.ts
@@ -0,0 +1,37 @@
+import request from 'supertest'
+import { HttpHeaderName, HttpStatusCode } from '@/presentation/constants'
+import { mockCompanyEntityDto } from '@/__tests__/domain/__mocks__/entities'
+import { addCompany, addUserAndAuthenticate, config, db, mongoInMemory, webServer } from '@/__tests__/shared/mongodb-server.utils'
+import { mockAuthorizationToken } from '@/__tests__/presentation/__mocks__'
+
+describe('UpdateCompany Route > PATCH /company', () => {
+  beforeAll(async () => {
+    await config()
+    await db().connect()
+  })
+
+  beforeEach(async () => {
+    await db().clearCollection('companies')
+    await db().clearCollection('users')
+    await webServer().listen()
+  })
+
+  afterEach(async () => {
+    await webServer().close()
+  })
+
+  afterAll(async () => {
+    await db().disconnect()
+    await mongoInMemory().stop()
+  })
+
+  it('shold return ok', async () => {
+    const { authenticatedUser, accessToken } = await addUserAndAuthenticate()
+    await addCompany(authenticatedUser)
+    await request(webServer().server())
+      .patch('/company')
+      .set(HttpHeaderName.AUTHORIZATION, mockAuthorizationToken(accessToken))
+      .send(mockCompanyEntityDto())
+      .expect(HttpStatusCode.OK)
+  })
+})
