2e1fe5904f6d42d9f362422cd5aa4081be74147e
William Fank Thomé <willegp88@gmail.com>
2020-11-25 20:10:21 -0300

    feat: create add building controller

---
+++ src/presentation/controllers/building/add-building.controller.ts
@@ -0,0 +1,31 @@
+// : Shared
+import { Inject, Injectable } from '@/shared/dependency-injection'
+// > In: presentation layer
+import { Controller, HandleResponse, HttpRequest } from '@/presentation/protocols'
+import { ok } from '@/presentation/factories/http.factory'
+import { buildingSchema } from '@/presentation/schemas'
+import { HandleLogError, ValidateBody } from '@/presentation/decorators'
+// < Out: only domain layer
+import { BuildingEntity, buildingKeys, CompanyEntity } from '@/domain/entities'
+import { AddBuildingUseCase } from '@/domain/usecases'
+import { BuildingDto } from '@/domain/protocols'
+
+@Injectable()
+export class AddBuildingController implements Controller<BuildingEntity> {
+
+  constructor (
+    @Inject() private readonly addBuildingUseCase: AddBuildingUseCase
+  ) { }
+
+  @ValidateBody<BuildingDto, BuildingEntity>({
+    schema: buildingSchema,
+    keys: buildingKeys
+  })
+  @HandleLogError
+  async handle (request: HttpRequest<BuildingDto>): HandleResponse<BuildingEntity> {
+    const companyId = request.activeCompanyInfo?.id as CompanyEntity['id']
+    const buildingDto = request.body as BuildingDto
+    const newBuilding = await this.addBuildingUseCase.call(buildingDto, companyId)
+    return ok(newBuilding)
+  }
+}
