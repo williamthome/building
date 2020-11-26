5198fecc3d232cbaf027558d558621e6fa9db9fa
William Fank Thomé <willegp88@gmail.com>
2020-11-26 12:41:30 -0300

    refactor: use omit instead of omit on indexed

--- src/presentation/protocols/entity-schema.protocol.ts
+++ src/presentation/protocols/entity-schema.protocol.ts
@@ -1,7 +1,6 @@
 import { Entity } from '@/domain/protocols'
-import { OmitOnIndexed } from '@/shared/types'
 import { Schema } from '../protocols'
 
-type EntitySchemaType<T extends Entity> = Required<OmitOnIndexed<T, 'id'>>
+type EntitySchemaType<T extends Entity> = Required<Omit<T, 'id'>>
 
 export type EntitySchema<T extends Entity> = Schema<EntitySchemaType<T>>
