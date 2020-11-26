060c5c03b780c8db877e86ed746ce2377cc16b0d
William Fank Thomé <willegp88@gmail.com>
2020-11-26 12:05:20 -0300

    refactor: create user rights interface

--- src/domain/protocols/user.protocol.ts
+++ src/domain/protocols/user.protocol.ts
@@ -1,5 +1,11 @@
-import { UserEntity } from '../entities'
+import { CompanyEntity, UserEntity } from '../entities'
+import { Member } from '../entities/nested'
 
 export type UserDto = Partial<Omit<UserEntity, 'id'>>
 
 export type AuthDto = Pick<UserEntity, 'email' | 'password'>
+
+export interface UserRights {
+  company: Pick<CompanyEntity, 'id' | 'members'>
+  rights: Omit<Member, 'userId'>
+}
