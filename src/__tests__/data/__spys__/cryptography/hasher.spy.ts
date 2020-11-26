1230d0fac8e51495a3854e07e5ba23e3cb50a4fe
William Fank Thomé <willegp88@gmail.com>
2020-11-26 14:05:24 -0300

    refactor: change hasher spy values to array

--- src/__tests__/data/__spys__/cryptography/hasher.spy.ts
+++ src/__tests__/data/__spys__/cryptography/hasher.spy.ts
@@ -1,16 +1,19 @@
 import { Hasher } from '@/data/protocols/cryptography'
 
 export class HasherSpy implements Hasher {
-  plaintext?: string
-  hashed?: string
+  plaintext: string[] = []
+  hashed: string[] = []
   shouldThrow = false
 
   hash = async (plaintext: string): Promise<string> => {
-    this.plaintext = plaintext
+    this.plaintext.push(plaintext)
 
     if (this.shouldThrow) throw new Error()
 
-    this.hashed = `${plaintext}_hashed`
-    return this.hashed
+    const hashed = `${plaintext}_hashed`
+
+    this.hashed.push(hashed)
+
+    return hashed
   }
 }
