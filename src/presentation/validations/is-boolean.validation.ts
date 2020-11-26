404d46ab8b0ba95e8c5f352884a6c040d71ea498
William Fank Thomé <willegp88@gmail.com>
2020-11-26 12:40:07 -0300

    feat: create is boolean validation

---
+++ src/presentation/validations/is-boolean.validation.ts
@@ -0,0 +1,25 @@
+import { makeValidationResult } from '../helpers/validation.helper'
+import { BaseValidation, Validation, ValidationResult } from '../protocols'
+
+class IsBooleanValidation extends BaseValidation<IsBooleanValidation> {
+  validation = (): IsBooleanValidation => this
+
+  validate = <T extends Record<string, unknown>> (
+    obj: T,
+    key: keyof T,
+    validations?: Validation[]
+  ): ValidationResult => {
+    const valid = key in obj && typeof obj[key] === 'boolean'
+    const errorMessage = `${this.isParam ? 'Param' : 'Field'} ${key} must be boolean type`
+    return makeValidationResult(
+      valid,
+      obj,
+      key,
+      errorMessage,
+      this.customErrorMessage,
+      validations
+    )
+  }
+}
+
+export const isBoolean = new IsBooleanValidation()
