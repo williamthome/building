e909bdc99534fa5feea1dcd1b98e5298768b765f
William Fank Thomé <willegp88@gmail.com>
2020-11-26 18:29:13 -0300

    refactor: inject encrypter and change return type to add user response

--- src/__tests__/data/contracts/user/add-user-contract.spec.ts
+++ src/__tests__/data/contracts/user/add-user-contract.spec.ts
@@ -1,6 +1,6 @@
 import container from '@/shared/dependency-injection'
 import { AddUserContract } from '@/data/contracts'
-import { AddUnverifiedRepositorySpy, AddUserRepositorySpy, HasherSpy } from '@/__tests__/data/__spys__'
+import { AddUnverifiedRepositorySpy, AddUserRepositorySpy, EncrypterSpy, HasherSpy } from '@/__tests__/data/__spys__'
 import { mockUserModelDto } from '@/__tests__/data/__mocks__/models'
 import { ModelDto } from '@/data/protocols'
 import { UserModel } from '@/data/models'
@@ -11,18 +11,21 @@ interface SutTypes {
   sut: AddUserContract
   hasherSpy: HasherSpy
   addUserRepositorySpy: AddUserRepositorySpy
+  encrypterSpy: EncrypterSpy
   addUnverifiedRepositorySpy: AddUnverifiedRepositorySpy
 }
 
 const makeSut = (): SutTypes => {
   const hasherSpy = container.resolve<HasherSpy>('hasher')
   const addUserRepositorySpy = container.resolve<AddUserRepositorySpy>('addUserRepository')
+  const encrypterSpy = container.resolve<EncrypterSpy>('encrypter')
   const addUnverifiedRepositorySpy = container.resolve<AddUnverifiedRepositorySpy>('addUnverifiedRepository')
   const sut = container.resolve(AddUserContract)
   return {
     sut,
     hasherSpy,
     addUserRepositorySpy,
+    encrypterSpy,
     addUnverifiedRepositorySpy
   }
 }
@@ -33,8 +36,24 @@ describe('AddUser Contract', () => {
   beforeEach(() => {
     container.define('hasher').asNewable(HasherSpy).done()
     container.define('addUserRepository').asNewable(AddUserRepositorySpy).done()
+    container.define('encrypter').asNewable(EncrypterSpy).done()
     container.define('addUnverifiedRepository').asNewable(AddUnverifiedRepositorySpy).done()
     container.define(AddUserContract).asNewable(AddUserContract).done()
+  })
+
+  describe('Hasher', () => {
+    it('should be called with right value', async () => {
+      const { sut, hasherSpy } = makeSut()
+      const dto = mockUserModelDto()
+      await sut.call(dto)
+      expect(hasherSpy.plaintext).toEqual(dto.password)
+    })
+
+    it('should throw if method throws', async () => {
+      const { sut, hasherSpy } = makeSut()
+      hasherSpy.shouldThrow = true
+      await expect(sut.call(mockUserModelDto())).rejects.toThrow()
+    })
   })
 
   describe('AddUser Repository', () => {
@@ -44,7 +63,7 @@ describe('AddUser Contract', () => {
       await sut.call(dto)
       const hashedDto: ModelDto<UserModel> = {
         ...dto,
-        password: hasherSpy.hashed[0],
+        password: hasherSpy.hashed,
         verified: false
       }
       expect(addUserRepositorySpy.userDto).toEqual(hashedDto)
@@ -57,26 +76,26 @@ describe('AddUser Contract', () => {
     })
   })
 
-  describe('Hasher', () => {
+  describe('Encrypter', () => {
     it('should be called with right value', async () => {
-      const { sut, hasherSpy } = makeSut()
+      const { sut, encrypterSpy, addUserRepositorySpy } = makeSut()
       const dto = mockUserModelDto()
       await sut.call(dto)
-      expect(hasherSpy.plaintext[0]).toEqual(dto.password)
+      expect(encrypterSpy.plaintext).toEqual(addUserRepositorySpy.userModel?.id)
     })
 
     it('should throw if method throws', async () => {
-      const { sut, hasherSpy } = makeSut()
-      hasherSpy.shouldThrow = true
+      const { sut, encrypterSpy } = makeSut()
+      encrypterSpy.shouldThrow = true
       await expect(sut.call(mockUserModelDto())).rejects.toThrow()
     })
   })
 
   describe('AddUnverified Repository', () => {
     it('should be called with right value', async () => {
-      const { sut, addUnverifiedRepositorySpy, hasherSpy } = makeSut()
+      const { sut, addUnverifiedRepositorySpy, addUserRepositorySpy } = makeSut()
       await sut.call(mockUserModelDto())
-      expect(addUnverifiedRepositorySpy.unverifiedDto?.token).toEqual(hasherSpy.hashed[1])
+      expect(addUnverifiedRepositorySpy.unverifiedDto?.userId).toEqual(addUserRepositorySpy.userModel?.id)
     })
 
     it('should throw if method throws', async () => {
@@ -87,9 +106,9 @@ describe('AddUser Contract', () => {
   })
 
   it('shold return a new user', async () => {
-    const { sut, addUserRepositorySpy } = makeSut()
-    const user = await sut.call(mockUserModelDto())
-    expect(user).toBeTruthy()
-    expect(user).toEqual(addUserRepositorySpy.userModel)
+    const { sut, addUserRepositorySpy, encrypterSpy } = makeSut()
+    const response = await sut.call(mockUserModelDto())
+    expect(response.user).toEqual(addUserRepositorySpy.userModel)
+    expect(response.verificationToken).toBe(encrypterSpy.encrypted)
   })
 })
--- src/data/contracts/user/add-user.contract.ts
+++ src/data/contracts/user/add-user.contract.ts
@@ -2,11 +2,11 @@
 import { Injectable, Inject } from '@/shared/dependency-injection'
 // > Data
 import { AddUserRepository, AddUnverifiedRepository } from '@/data/repositories'
-import { Hasher } from '@/data/protocols/cryptography'
+import { Encrypter, Hasher } from '@/data/protocols/cryptography'
 // < Only Domain
 import { UserEntity } from '@/domain/entities'
 import { AddUserUseCase } from '@/domain/usecases'
-import { UserDto } from '@/domain/protocols'
+import { AddUserResponse, UserDto } from '@/domain/protocols'
 
 @Injectable('addUserUseCase')
 export class AddUserContract implements AddUserUseCase {
@@ -14,10 +14,11 @@ export class AddUserContract implements AddUserUseCase {
   constructor (
     @Inject() private readonly hasher: Hasher,
     @Inject() private readonly addUserRepository: AddUserRepository,
-    @Inject() private readonly addUnverifiedRepository: AddUnverifiedRepository
+    @Inject() private readonly addUnverifiedRepository: AddUnverifiedRepository,
+    @Inject() private readonly encrypter: Encrypter
   ) {}
 
-  call = async (userDto: UserDto): Promise<UserEntity> => {
+  call = async (userDto: UserDto): Promise<AddUserResponse> => {
     const hashedPassword = await this.hasher.hash(userDto.password as UserEntity['password'])
     const user = await this.addUserRepository.addUser({
       ...userDto,
@@ -27,13 +28,14 @@ export class AddUserContract implements AddUserUseCase {
 
     const tomorrow = new Date()
     tomorrow.setDate(tomorrow.getDate() + 1)
-    const verificationToken = await this.hasher.hash(user.id)
 
     await this.addUnverifiedRepository.addUnverified({
-      token: verificationToken,
+      userId: user.id,
       expiresIn: tomorrow.getTime()
     })
 
-    return user
+    const verificationToken = await this.encrypter.encrypt(user.id)
+
+    return { user, verificationToken }
   }
 }
