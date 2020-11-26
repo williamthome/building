0fa5e5dd9f9608b9048136c9c6daa172beed7043
William Fank Thomé <willegp88@gmail.com>
2020-11-26 12:09:47 -0300

    refactor: delete user companies if the user is the unique company owner

--- src/data/contracts/user/delete-user.contract.ts
+++ src/data/contracts/user/delete-user.contract.ts
@@ -1,19 +1,38 @@
 // : Shared
 import { Injectable, Inject } from '@/shared/dependency-injection'
 // > Data
-import { DeleteUserRepository } from '@/data/repositories'
+import { DeleteUserRepository, GetUserRightsRepository } from '@/data/repositories'
 // < Only Domain
 import { UserEntity } from '@/domain/entities'
-import { DeleteUserUseCase } from '@/domain/usecases'
+import { DeleteCompanyUseCase, DeleteUserUseCase } from '@/domain/usecases'
+import { CompanyRole } from '@/shared/constants'
 
 @Injectable('deleteUserUseCase')
 export class DeleteUserContract implements DeleteUserUseCase {
 
   constructor (
-    @Inject() private readonly deleteUserRepository: DeleteUserRepository
+    @Inject()
+    private readonly deleteUserRepository: DeleteUserRepository,
+
+    @Inject()
+    private readonly getUserRightsRepository: GetUserRightsRepository,
+
+    @Inject()
+    private readonly deleteCompanyUseCase: DeleteCompanyUseCase
   ) {}
 
   call = async (userId: UserEntity['id']): Promise<UserEntity | null> => {
-    return await this.deleteUserRepository.deleteUser(userId)
+    const user = await this.deleteUserRepository.deleteUser(userId)
+    if (!user) return null
+
+    const userRights = await this.getUserRightsRepository.getUserRights(userId)
+    for (const { company, rights } of userRights) {
+      const ownerCount = company.members.filter(member => userId === member.userId).length
+      if (ownerCount === 1 && rights.companyRole === CompanyRole.owner) {
+        await this.deleteCompanyUseCase.call(company.id)
+      }
+    }
+
+    return user
   }
 }
