e9dd5a4bf9ca7c7f8d9285c829605213f4c52ac5
William Fank Thomé <willegp88@gmail.com>
2020-11-27 11:38:41 -0300

    fix: prevent duplicated user register

--- src/__tests__/presentation/controllers/user/add-user-controller.spec.ts
+++ src/__tests__/presentation/controllers/user/add-user-controller.spec.ts
@@ -1,12 +1,13 @@
 import container from '@/shared/dependency-injection'
 // > In: presentation layer
 import { AddUserController } from '@/presentation/controllers'
-import { ok, serverError } from '@/presentation/factories/http.factory'
+import { badRequest, ok, serverError } from '@/presentation/factories/http.factory'
 import { HttpRequest } from '@/presentation/protocols'
 import { mockUserEntityDto } from '@/__tests__/domain/__mocks__/entities'
-import { AddUserUseCaseSpy } from '@/__tests__/domain/__spys__/usecases'
+import { AddUserUseCaseSpy, GetUserByEmailUseCaseSpy } from '@/__tests__/domain/__spys__/usecases'
 // < Out: only domain layer
 import { UserEntity } from '@/domain/entities'
+import { UserAlreadyRegisteredError } from '@/presentation/errors'
 
 //#region Factories
 
@@ -17,14 +18,17 @@ const mockHttpRequest = (): HttpRequest<Partial<Omit<UserEntity, 'id'>>> => ({
 
 interface SutTypes {
   sut: AddUserController
+  getUserByEmailUseCaseSpy: GetUserByEmailUseCaseSpy
   addUserUseCaseSpy: AddUserUseCaseSpy
 }
 
 const makeSut = (): SutTypes => {
+  const getUserByEmailUseCaseSpy = container.resolve<GetUserByEmailUseCaseSpy>('getUserByEmailUseCase')
   const addUserUseCaseSpy = container.resolve<AddUserUseCaseSpy>('addUserUseCase')
   const sut = container.resolve(AddUserController)
   return {
     sut,
+    getUserByEmailUseCaseSpy,
     addUserUseCaseSpy
   }
 }
@@ -33,19 +37,43 @@ const makeSut = (): SutTypes => {
 
 describe('AddUser Controller', () => {
   beforeEach(() => {
+    container.define('getUserByEmailUseCase').asNewable(GetUserByEmailUseCaseSpy).done()
     container.define('addUserUseCase').asNewable(AddUserUseCaseSpy).done()
     container.define(AddUserController).asNewable(AddUserController).done()
   })
 
   describe('AddUser UseCase', () => {
     it('should been called with right values', async () => {
-      const { sut, addUserUseCaseSpy } = makeSut()
+      const { sut, getUserByEmailUseCaseSpy } = makeSut()
+      await sut.handle(mockHttpRequest())
+      expect(getUserByEmailUseCaseSpy.email).toEqual(userDto.email)
+    })
+
+    it('should return server error if throws', async () => {
+      const { sut, getUserByEmailUseCaseSpy } = makeSut()
+      getUserByEmailUseCaseSpy.shouldThrow = true
+      const response = await sut.handle(mockHttpRequest())
+      expect(response).toEqual(serverError(new Error()))
+    })
+
+    it('should return server error if throws', async () => {
+      const { sut, } = makeSut()
+      const response = await sut.handle(mockHttpRequest())
+      expect(response).toEqual(badRequest(new UserAlreadyRegisteredError()))
+    })
+  })
+
+  describe('AddUser UseCase', () => {
+    it('should been called with right values', async () => {
+      const { sut, getUserByEmailUseCaseSpy, addUserUseCaseSpy } = makeSut()
+      getUserByEmailUseCaseSpy.shouldReturnNull = true
       await sut.handle(mockHttpRequest())
       expect(addUserUseCaseSpy.userDto).toEqual(userDto)
     })
 
     it('should return server error if throws', async () => {
-      const { sut, addUserUseCaseSpy } = makeSut()
+      const { sut, getUserByEmailUseCaseSpy, addUserUseCaseSpy } = makeSut()
+      getUserByEmailUseCaseSpy.shouldReturnNull = true
       addUserUseCaseSpy.shouldThrow = true
       const response = await sut.handle(mockHttpRequest())
       expect(response).toEqual(serverError(new Error()))
@@ -53,7 +81,8 @@ describe('AddUser Controller', () => {
   })
 
   it('shold return ok with a new user on body', async () => {
-    const { sut, addUserUseCaseSpy } = makeSut()
+    const { sut, getUserByEmailUseCaseSpy, addUserUseCaseSpy } = makeSut()
+    getUserByEmailUseCaseSpy.shouldReturnNull = true
     const response = await sut.handle(mockHttpRequest())
     expect(response).toEqual(ok(addUserUseCaseSpy.userVerificationToken))
   })
--- src/presentation/controllers/user/add-user.controller.ts
+++ src/presentation/controllers/user/add-user.controller.ts
@@ -2,18 +2,20 @@
 import { Inject, Injectable } from '@/shared/dependency-injection'
 // > In: presentation layer
 import { Controller, HandleResponse, HttpRequest } from '@/presentation/protocols'
-import { ok } from '@/presentation/factories/http.factory'
+import { badRequest, ok } from '@/presentation/factories/http.factory'
 import { userSchema } from '@/presentation/schemas'
 import { HandleLogError, ValidateBody } from '@/presentation/decorators'
+import { UserAlreadyRegisteredError } from '@/presentation/errors'
 // < Out: only domain layer
 import { userKeys } from '@/domain/entities'
-import { AddUserUseCase } from '@/domain/usecases'
+import { AddUserUseCase, GetUserByEmailUseCase } from '@/domain/usecases'
 import { UserVerificationToken, UserDto } from '@/domain/protocols'
 
 @Injectable()
 export class AddUserController implements Controller<UserDto, UserVerificationToken> {
 
   constructor (
+    @Inject() private readonly getUserByEmailUseCase: GetUserByEmailUseCase,
     @Inject() private readonly addUserUseCase: AddUserUseCase
   ) { }
 
@@ -24,6 +26,11 @@ export class AddUserController implements Controller<UserDto, UserVerificationTo
   @HandleLogError
   async handle (request: HttpRequest<UserDto>): HandleResponse<UserVerificationToken> {
     const userDto = request.body as UserDto
+
+    const user = await this.getUserByEmailUseCase.call(userDto.email as string)
+    if (user)
+      return badRequest(new UserAlreadyRegisteredError())
+
     const { user: userWithoutPassword, verificationToken } = await this.addUserUseCase.call(userDto)
     return ok({
       user: userWithoutPassword,
