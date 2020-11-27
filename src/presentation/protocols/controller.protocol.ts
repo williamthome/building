43bef8147204b0c55530986f383c10905c98ca5b
William Fank Thomé <willegp88@gmail.com>
2020-11-27 20:39:29 -0300

    refactor: include optional uses transaction field for controller interface

--- src/presentation/protocols/controller.protocol.ts
+++ src/presentation/protocols/controller.protocol.ts
@@ -3,5 +3,6 @@ import { HttpRequest, HttpResponse } from './http.protocol'
 export type HandleResponse<T = null> = Promise<HttpResponse<T | null | Error>>
 
 export interface Controller<TRequest, TResponse = TRequest> {
+  usesTransaction?: boolean
   handle: (request: HttpRequest<TRequest>) => HandleResponse<TResponse>
 }
