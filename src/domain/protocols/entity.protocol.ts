52670524dfde1308292a3fbab2fd77335d42ced9
William Fank Thomé <willegp88@gmail.com>
2020-11-29 08:37:00 -0300

    refactor: include plan in entity name type

--- src/domain/protocols/entity.protocol.ts
+++ src/domain/protocols/entity.protocol.ts
@@ -2,4 +2,4 @@ export interface Entity {
   id: string
 }
 
-export type EntityName = 'User' | 'Company' | 'Building' | 'Project' | 'Unverified'
+export type EntityName = 'User' | 'Company' | 'Building' | 'Project' | 'Unverified' | 'Plan'
