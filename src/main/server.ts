2737bd09e7177fae0f024e78e8cb47c88f92d6ea
William Fank Thomé <willegp88@gmail.com>
2020-11-30 13:26:41 -0300

    refactor: import storages and inject aws variables

--- src/main/server.ts
+++ src/main/server.ts
@@ -8,6 +8,7 @@ export class Server {
 
   private importDepencencyInjections = async (): Promise<void> => {
     await import('@/infra/databases')
+    await import('@/infra/storages')
     await import('@/infra/repositories')
     await import('@/data/adapters/cryptography')
     await import('@/data/contracts')
@@ -22,6 +23,10 @@ export class Server {
     container.define('PORT').as(5051).done()
     container.define('DB_URL').as('mongodb://localhost:27001,localhost:27002,localhost:27003/building').done()
     container.define('JWT_SECRET').as('building_jwt_secret').done()
+    container.define('AWS_ACCESS_KEY_ID').as('AKIAIMYTQYTNOPTU3DVA').done()
+    container.define('AWS_SECRET_ACCESS_KEY').as('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').done()
+    container.define('AWS_BUCKET').as('building-app-bucket').done()
+    container.define('AWS_REGION').as('us-east-1').done()
   }
 
   public config = async (): Promise<Server> => {
