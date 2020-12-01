b9b8fbe5cb30c40f106e70d59cfc61bddb032b72
William Fank Thomé <willegp88@gmail.com>
2020-12-01 14:32:32 -0300

    feat: create presentation file protocol

---
+++ src/presentation/protocols/file.protocol.ts
@@ -0,0 +1,21 @@
+export interface RequestFile {
+  name: string
+  buffer: Buffer
+  mimeType: string
+}
+
+export const requestFileKeys: Array<keyof RequestFile> = [
+  'name',
+  'buffer',
+  'mimeType',
+]
+
+interface UploadErrorResponse {
+  fileName: RequestFile['name'],
+  errorMessage: Error['message']
+}
+
+export interface UploadFileResponse {
+  uploads: string[]
+  errors: UploadErrorResponse[]
+}
