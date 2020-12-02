2404dae1d4c1750b8a8d6102f78aa87666ed00f0
William Fank Thomé <willegp88@gmail.com>
2020-12-02 18:06:59 -0300

    feat: create keys types and a helper

---
+++ src/shared/helpers/key.helper.ts
@@ -0,0 +1,13 @@
+import { IsUnique, OptionalLiteralKeys, RequiredLiteralKeys } from '../types/keys'
+
+export class UniqueKeysOf<T> {
+  all = <K extends Array<keyof T>> (...properties: K & IsUnique<K>): K => {
+    return properties
+  }
+  optinal = <K extends Array<OptionalLiteralKeys<T>>> (...properties: K & IsUnique<K>): K => {
+    return properties
+  }
+  required = <K extends Array<RequiredLiteralKeys<T>>> (...properties: K & IsUnique<K>): K => {
+    return properties
+  }
+}
