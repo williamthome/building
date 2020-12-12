10cde1016a6345ee0f9047ecb94942e4134bb081
William Fank Thomé <willegp88@gmail.com>
2020-12-12 13:08:46 -0300

    refactor: add participant ids field in project schema

--- src/domain/entities/project.entity.ts
+++ src/domain/entities/project.entity.ts
@@ -1,12 +1,13 @@
 import { ProjectStatus } from '@/shared/constants'
 import { OmitKey } from '@/shared/types'
 import { ExtractDto, ExtractEntity, ExtractUpdateDto, limitedEntitySchema } from '../protocols/entity.protocol'
-import { string, number } from '../protocols/schema'
+import { string, number, array } from '../protocols/schema'
 import { CreateFileDto } from './file.entity'
 
 export const projectSchema = limitedEntitySchema({
   buildingId: string().required(),
   phaseId: string().required(),
+  participantIds: array<string>().required().minSize(1),
   title: string().required(),
   status: number<ProjectStatus>()
 })
