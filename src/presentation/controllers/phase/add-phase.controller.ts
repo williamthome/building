61f13d9d4290525007d898b29a2f6a01fe136d19
William Fank Thomé <willegp88@gmail.com>
2020-12-12 12:54:13 -0300

    refactor: filter unique participant ids and verify if all participants are member

--- src/presentation/controllers/phase/add-phase.controller.ts
+++ src/presentation/controllers/phase/add-phase.controller.ts
@@ -1,13 +1,16 @@
 // : Shared
 import { Inject } from '@/shared/dependency-injection'
+import { uniqueItems } from '@/shared/helpers/array.helper'
 // > In: presentation layer
 import { Controller, HandleResponse, HttpRequest } from '@/presentation/protocols'
-import { notFound, ok } from '@/presentation/factories/http.factory'
+import { badRequest, notFound, ok } from '@/presentation/factories/http.factory'
 import { HandleError, InjectableController, Validate } from '@/presentation/decorators'
+import { EntityNotFoundError, UserIsNotAMemberError } from '@/presentation/errors'
 // < Out: only domain layer
 import { AddPhaseUseCase, GetBuildingByIdUseCase } from '@/domain/usecases'
 import { Phase, Company, phaseSchema, CreatePhaseDto } from '@/domain/entities'
-import { EntityNotFoundError } from '@/presentation/errors'
+import { Member } from '@/domain/entities/nested'
+import { userIsMember } from '@/domain/helpers/user.helper'
 
 @InjectableController()
 export class AddPhaseController implements Controller<CreatePhaseDto, Phase> {
@@ -29,12 +32,21 @@ export class AddPhaseController implements Controller<CreatePhaseDto, Phase> {
   })
   async handle (request: HttpRequest<CreatePhaseDto>): HandleResponse<Phase> {
     const activeCompanyId = request.activeCompanyInfo?.id as Company['id']
+    const activeCompanyMembers = request.activeCompanyInfo?.members as Member[]
     const createPhaseDto = request.body as CreatePhaseDto
-    const { buildingId } = createPhaseDto
+
+    createPhaseDto.participantIds = uniqueItems(createPhaseDto.participantIds)
+
+    const { buildingId, participantIds } = createPhaseDto
 
     const findedBuilding = await this.getBuildingByIdUseCase.call(buildingId)
     if (!findedBuilding)
       return notFound(new EntityNotFoundError('Building'))
+
+    for (const participantId of participantIds) {
+      if (!userIsMember(activeCompanyMembers, participantId))
+        return badRequest(new UserIsNotAMemberError())
+    }
 
     const createdPhase = await this.addPhaseUseCase.call(createPhaseDto, activeCompanyId)
 
