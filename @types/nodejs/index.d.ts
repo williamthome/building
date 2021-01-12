46d51444f676911557c985cdce04c24a942a0094
William Fank Thomé <willegp88@gmail.com>
2021-01-12 20:43:35 -0300

    refactor: add vars to node env

--- @types/nodejs/index.d.ts
+++ @types/nodejs/index.d.ts
@@ -3,6 +3,12 @@ declare global {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production' | 'test'
       PORT: string
+      JWT_SECRET: string
+      MONGO_URL: string
+      AWS_ACCESS_KEY_ID: string
+      AWS_SECRET_ACCESS_KEY: string
+      AWS_BUCKET: string
+      AWS_REGION: string
     }
   }
 }
--- src/main/server.ts
+++ src/main/server.ts
@@ -19,16 +19,43 @@ export class Server {
   }
 
   private defineInjectedValues = (): void => {
-    container.define('PORT').as(5051).done()
+    const {
+      PORT,
+      MONGO_URL,
+      JWT_SECRET,
+      AWS_ACCESS_KEY_ID,
+      AWS_SECRET_ACCESS_KEY,
+      AWS_BUCKET,
+      AWS_REGION
+    } = process.env
+    container
+      .define('PORT')
+      .as(PORT || 5051)
+      .done()
     container
       .define('DB_URL')
-      .as('mongodb://localhost:27001,localhost:27002,localhost:27003/building')
+      .as(MONGO_URL || 'mongodb://localhost:27001,localhost:27002,localhost:27003/building')
       .done()
-    container.define('JWT_SECRET').as('building_jwt_secret').done()
-    container.define('AWS_ACCESS_KEY_ID').as('AKIAIMYTQYTNOPTU3DVA').done()
-    container.define('AWS_SECRET_ACCESS_KEY').as('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').done()
-    container.define('AWS_BUCKET').as('building-app-bucket').done()
-    container.define('AWS_REGION').as('us-east-1').done()
+    container
+      .define('JWT_SECRET')
+      .as(JWT_SECRET || 'building_jwt_secret')
+      .done()
+    container
+      .define('AWS_ACCESS_KEY_ID')
+      .as(AWS_ACCESS_KEY_ID || 'AKIAIMYTQYTNOPTU3DVA')
+      .done()
+    container
+      .define('AWS_SECRET_ACCESS_KEY')
+      .as(AWS_SECRET_ACCESS_KEY || 'EGwKAKSUa7D6lvNntSiYIW0r70Yp6CXU1GYGxZ2w')
+      .done()
+    container
+      .define('AWS_BUCKET')
+      .as(AWS_BUCKET || 'building-app-bucket')
+      .done()
+    container
+      .define('AWS_REGION')
+      .as(AWS_REGION || 'us-east-1')
+      .done()
   }
 
   public config = async (): Promise<Server> => {
